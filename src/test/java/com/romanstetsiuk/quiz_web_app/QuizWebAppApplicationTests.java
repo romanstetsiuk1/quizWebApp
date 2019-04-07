d90b550d69ffb6b2350f60f6602ef39f4df0661c
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-07 11:48:41 +0200

    Create project

---
+++ src/test/java/com/romanstetsiuk/quiz_web_app/QuizWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.romanstetsiuk.quiz_web_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizWebAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
